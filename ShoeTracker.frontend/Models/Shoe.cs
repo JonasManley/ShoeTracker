368cb6771b3507f25591ceb281a4dee0cf163804
JonasManley <54889189+JonasManley@users.noreply.github.com>
2024-08-27 22:17:16 +0200

    Shoe overview page

---
+++ ShoeTracker.frontend/Models/Shoe.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoeTracker.frontend.Models
+{
+    public class Shoe
+    {
+        public string Name { get; set; }
+        public int Kilometers { get; set; }
+        public int MaxKilometers { get; set; }
+        public int UsagePercentage => (int)((double)Kilometers / MaxKilometers * 100);
+    }
+}
